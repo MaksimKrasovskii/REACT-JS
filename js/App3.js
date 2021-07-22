7f24ae645aa0051640d1e3fb12b2ddf5c4d78c17
pages <maksim.krasovskii@yandex.ru>
2021-07-22 18:04:45 +0300

    код

---
+++ js/App3.js
@@ -0,0 +1,13 @@
+/*import React from "react";*/
+
+
+const styles = {
+    fontFamily: "sans-serif",
+    textAlign: "center"
+};
+
+class App3 extends React.Component {
+    render() {
+        return <div style={styles}>{this.props.z1}</div>;
+    }
+}
